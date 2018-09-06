f13a0e34e0a28dea99115787bfead3430be1d5ea
chemzqm <chemzqm@gmail.com>
2018-09-06 18:52:38 +0800

    fix css completion

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { ExtensionContext, LanguageClient, ServerOptions, workspace, services, TransportKind, LanguageClientOptions, WorkspaceConfiguration } from 'coc.nvim'
+import { ExtensionContext, LanguageClient, ServerOptions, workspace, services, TransportKind, LanguageClientOptions, WorkspaceConfiguration, ProvideCompletionItemsSignature } from 'coc.nvim'
+import { TextDocument, Position, CompletionItem, CompletionList, InsertTextFormat } from 'vscode-languageserver-protocol'
+import { CompletionContext } from 'vscode-languageserver-protocol';
+import { CancellationToken } from 'vscode-jsonrpc';
+import { ProviderResult } from 'coc.nvim/lib/provider';
 
 const sections = ['vetur', 'emmet', 'html', 'javascript', 'typescript', 'prettier', 'stylusSupremacy']
 
@@ -43,6 +47,33 @@ export async function activate(context: ExtensionContext): Promise<void> {
     outputChannelName: 'vetur',
     initializationOptions: {
       config: getConfig(c)
+    },
+    middleware: {
+      provideCompletionItem: (
+        document: TextDocument,
+        position: Position,
+        context: CompletionContext,
+        token: CancellationToken,
+        next: ProvideCompletionItemsSignature
+      ): ProviderResult<CompletionItem[] | CompletionList> => {
+        return Promise.resolve(next(document, position, context, token)).then((res: CompletionItem[] | CompletionList) => {
+          let doc = workspace.getDocument(document.uri)
+          if (!doc) return []
+          let items: CompletionItem[] = res.hasOwnProperty('isIncomplete') ? (res as CompletionList).items : res as CompletionItem[]
+          let pre = doc.getline(position.line).slice(0, position.character)
+          // searching for class name
+          if (/(^|\s)\.\w*$/.test(pre)) {
+            items = items.filter(o => o.label.startsWith('.'))
+            items.forEach(fixItem)
+          }
+          if (context.triggerCharacter == ':'
+            || /\:\w*$/.test(pre)) {
+            items = items.filter(o => o.label.startsWith(':'))
+            items.forEach(fixItem)
+          }
+          return items
+        })
+      }
     }
   }
 
@@ -51,4 +82,13 @@ export async function activate(context: ExtensionContext): Promise<void> {
   subscriptions.push(
     services.registLanguageClient(client)
   )
+}
+
+
+function fixItem(item: CompletionItem): void {
+  item.data = item.data || {}
+  item.data.abbr = item.label
+  item.label = item.label.slice(1)
+  item.textEdit = null
+  item.insertTextFormat = InsertTextFormat.PlainText
 }
